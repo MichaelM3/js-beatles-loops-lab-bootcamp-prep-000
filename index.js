4e032b19e4677f195aa0d1ef3856363f9bfdb609
Michael Muniz <michael.muniz94@gmail.com>
2018-09-13 00:31:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,18 +17,11 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
 function johnLennonFacts(array) {
   var i = 0;
   var newFacts = [];
-  while (facts.length > i) {
-    newFacts.push(`${facts[i]}!!!`);
+  while (array.length > i) {
+    newFacts.push(`${array[i]}!!!`);
     i++;
   }
   return newFacts;
